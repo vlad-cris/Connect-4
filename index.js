4909e05d4dbfb6cd0b6f6e071be876adadad2e8d
Vlad Haba <vlad02cristi@yahoo.com>
2023-11-01 20:54:23 +0100

    Update with winner check and restart functionality

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let boardMatrix = new Array();
 let playerColors = ["bg-danger", "bg-primary"];
 let playerRound = 1;
 let gameCount = 0;
+let endGame = false;
 
 createBoardMatrix(7, 6, boardMatrix);
 createBoardElements();
@@ -22,7 +23,6 @@ function createBoardElements() {
         }
     }
 };
-
 
 //update player round
 function updatePlayerRound() {
@@ -60,11 +60,100 @@ function createOneBoxElement(id) {
 
 // function to do at onclick on board
 function onclickBoard(id) {
-    if (document.getElementById(id).classList.contains("bg-light")) {
+    if (document.getElementById(id).classList.contains("bg-light") && !endGame) {
         addColor(id);
         updateMatrixValues(id);
-        updatePlayerRound();
         ++gameCount;
+        checkGame(id);
+        updatePlayerRound();
+    }
+};
+
+//function to check game board values
+function checkGame(id) {
+    if (gameCount == 42) {
+        endGame = true;
+        showWinner("The GAME IS OVER!")
+    }
+    checkHorizontally(id);
+    checkVertical(id);
+    checkOnPrincipalDiagonal(id);
+    checkOnSecondaryDiagonal(id);
+};
+
+
+// functions in check game
+function checkHorizontally(id) {
+    let elementsCount = 0;
+    let row = Math.floor(id / 7);
+    for (let i = id % 7; i >= 0 && boardMatrix[row][i] === playerRound; i--) {
+        elementsCount++;
+    }
+    for (let i = id % 7 + 1; i < 7 && boardMatrix[row][i] === playerRound; i++) {
+        elementsCount++;
+    }
+    if (elementsCount > 3) {
+        endGame = true;
+        showWinner(`The winner is Player ${playerRound + 1}`);
+    }
+};
+
+function checkVertical(id) {
+    let elementsCount = 0;
+    let col = id % 7;
+    for (let i = Math.floor(id / 7); i >= 0 && boardMatrix[i][col] === playerRound; i--) {
+        elementsCount++;
+    }
+    for (let i = Math.floor(id / 7) + 1; i < 6 && boardMatrix[i][col] === playerRound; i++) {
+        elementsCount++;
+    }
+    if (elementsCount > 3) {
+        endGame = true;
+        showWinner(`The winner is Player ${playerRound + 1}`);
+    }
+};
+
+function checkOnPrincipalDiagonal(id) {
+    let elementsCount = 0;
+    let col = id % 7;
+    let row = Math.floor(id / 7);
+    while (col >= 0 && row >= 0 && boardMatrix[row][col] === playerRound) {
+        elementsCount++;
+        col--;
+        row--;
+    }
+    col = id % 7 + 1;
+    row = Math.floor(id / 7) + 1;
+    while (col < 7 && row < 6 && boardMatrix[row][col] === playerRound) {
+        elementsCount++;
+        col++;
+        row++;
+    }
+    if (elementsCount > 3) {
+        endGame = true;
+        showWinner(`The winner is Player ${playerRound + 1}`);
+    }
+};
+
+function checkOnSecondaryDiagonal(id) {
+    let elementsCount = 0;
+    let col = id % 7;
+    let row = Math.floor(id / 7);
+    while (col >= 0 && row < 6 && boardMatrix[row][col] === playerRound) {
+        elementsCount++;
+        col--;
+        row++;
+    }
+    col = id % 7 + 1;
+    row = Math.floor(id / 7) - 1;
+    while (col < 7 && row >= 0 && boardMatrix[row][col] === playerRound) {
+        elementsCount++;
+        col++;
+        row--;
+    }
+    if (elementsCount > 3) {
+        endGame = true;
+        showWinner(`The winner is Player ${playerRound + 1}`);
     }
 };
 
@@ -72,4 +161,37 @@ function onclickBoard(id) {
 function addColor(id) {
     document.getElementById(id).classList.remove("bg-light");
     document.getElementById(id).classList.add(playerColors[playerRound]);
-}
+};
+
+// restart the game
+function restart() {
+    playerRound = 1;
+    gameCount = 0;
+    endGame = false;
+    restartBoard();
+    updatePlayerRound();
+    showPlayerRound();
+};
+
+// winner message
+function showWinner(message) {
+    document.getElementById("round").setAttribute("hidden", "");
+    document.getElementById("winner").removeAttribute("hidden");
+    document.getElementById("restart").removeAttribute("hidden");
+    document.getElementById("winner").innerText = message;
+};
+
+// show game board
+function showPlayerRound() {
+    document.getElementById("round").removeAttribute("hidden");
+    document.getElementById("winner").setAttribute("hidden", "");
+    document.getElementById("restart").setAttribute("hidden", "");
+};
+
+// reset board in html and in matrix
+function restartBoard() {
+    boardMatrix = new Array();
+    createBoardMatrix(7, 6, boardMatrix);
+    boardElements.innerHTML = "";
+    createBoardElements();
+};
